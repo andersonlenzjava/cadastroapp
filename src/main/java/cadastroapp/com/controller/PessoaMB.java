5affad013c2fe7768442cba9f1fc837fd8a89c9f
Anderson Miguel Lenz <105016572+andersonlenzjava@users.noreply.github.com>
2023-08-07 07:57:15 -0300

    Tentativa navegação entre páginas

---
+++ src/main/java/cadastroapp/com/controller/PessoaMB.java
@@ -0,0 +1,61 @@
+package cadastroapp.com.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cadastroapp.com.model.Pessoa;
+
+// equivalente a uma controller 
+// o acionamento dos métodos é feito na view
+// arquitetura orientada a eventos
+// escopos estabelecem limites relacionados ao transporte das informaçoes (statefull)
+@Named("bean")
+@ApplicationScoped
+public class PessoaMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Pessoa pessoa;
+	
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>(); 
+	
+	public String adicionar() {
+		
+		pessoas.add(pessoa);
+		
+		// limpar os campos que recebem dados após uma operação
+		limpar();
+		
+		return null;
+	}
+	
+	private void limpar() {
+		
+		pessoa = new Pessoa();
+		
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	
+}
---
+++ src/main/java/cadastroapp/com/model/Pessoa.java
@@ -0,0 +1,55 @@
+package cadastroapp.com.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
